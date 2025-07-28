38cde7b925bbc37a79b804e9cd5769d881f68ac7
Scott <sc2455@cam.ac.uk>
2025-07-28 15:34:24 +0100

    Remove comments from files

--- start_runfile.sh
+++ start_runfile.sh
@@ -1,7 +1,3 @@
-
 source /home/sc2455/Documents/venv/bin/activate
 
-#/home/sc2455/Documents/venv/bin/pip install mcp3008
-
 /home/sc2455/Documents/venv/bin/python3 /home/sc2455/Documents/runfile.py >> /home/sc2455/Documents/runlog.txt 2>&1
-
